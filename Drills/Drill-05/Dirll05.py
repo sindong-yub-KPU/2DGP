484936edfc8f82dba8010e20c0941fde65cd7ef0
sindong-kpu <rch502@naver.com>
2018-09-17 15:01:07 +0900

    201518021
    2차 커밋

--- Drills/Drill-05/Dirll05.py
+++ Drills/Drill-05/Dirll05.py
@@ -6,15 +6,38 @@ gox = True
 goy = True
 open_canvas()
 character = load_image('animation_sheet.png')
-x = 203
-y = 535
+x = 132
+y = 243
 frame = 0
 # 0 이면 + + 1 이면 - - 2 이면 - + 3 이면 + -
 count = 0
 def one_point ():
 
+    global direction , count , direction , gox , goy , x ,y
+
+    if count == 0:
+        direction = 1
+        if x <= 132 :
+            gox = False
+        if y <= 243 :
+            goy = False
+        if(gox == False and goy == False ):
+            count = 1
+            gox = True
+            goy = True
     pass
 def two_point ():
+    global direction, count, direction, gox, goy, x, y
+    if count == 1 :
+        direction = 0
+        if x >= 535 :
+            gox = False
+        if y >= 470 :
+            goy = False
+        if (gox == False and goy == False):
+            count = 2
+            gox = True
+            goy = True
     pass
 def three_point():
     pass
@@ -34,11 +57,36 @@ def return_point():
     pass
 
 def direction_point():
+    global direction
     pass
-
+def goto_x_y():
+    global direction , count , direction , gox , goy ,x ,y
+    if direction == 0:
+        if gox == True:
+            x += 2
+        if goy == True:
+            y += 2
+    elif direction == 1:
+        if gox == True:
+            x -= 2
+        if goy == True:
+            y -= 2
+    elif direction == 2:
+        if gox == True:
+            x -= 2
+        if goy == True:
+            y += 2
+    elif direction == 3:
+        if gox == True:
+            x += 2
+        if goy == True:
+            y -= 2
+pass
 while running:
     clear_canvas()
-    one_point()
+    #one_point()
+    two_point()
+    goto_x_y()
 
     character.clip_draw(frame * 100, 100 * right_or_left, 100, 100, x, y)
     delay(0.02)
