5577604f3e03ee7251bb177e93743be6b021b958
sindong-kpu <rch502@naver.com>
2018-10-15 19:03:05 +0900

    Drill10 - 1
    2차커밋
    2015180021 신동엽

--- Drills/Drill-10/pause_state.py
+++ Drills/Drill-10/pause_state.py
@@ -18,13 +18,17 @@ def exit():
     del(image)
     pass
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type , event.key) == (SDL_KEYDOWN , SDLK_ESCAPE):
+                game_framework.change_state(title_state)
+            elif (event.type , event.key) == (SDL_KEYDOWN , SDLK_p):
+                game_framework.pop_state()
 def update():
     pass
-
-
-
-
 def pause():
     pass
 
@@ -34,4 +38,3 @@ def resume():
 
 def draw():
     pass
-
