3bb1352bd7bc158151ec3235822ea1675abfffc0
sindong-kpu <rch502@naver.com>
2018-12-01 00:30:16 +0900

    2D 프로젝트
    스테이지1 식물심기 추가

--- 2DProject/2DResource/Stage1.py
+++ 2DProject/2DResource/Stage1.py
@@ -474,6 +474,8 @@ class Stage_level_1:
             self.time_bar_image = load_image('Stage1/progress_bar.png')
         self.ZombiescomingSound = load_wav('Gamesoundeffect/Zombiescoming.wav')
         self.ZombiescomingSound.set_volume(64)
+        self.Planting_plant = load_wav('Gamesoundeffect/plant1.wav')
+        self.Planting_plant.set_volume(64)
         self.zombiecome = False
         self.time_bar = 0
         self.intro_music.set_volume(32)  # 스테이지 들어오면 음악이 바로 재생되게함
@@ -538,6 +540,7 @@ class Stage_level_1:
                                 self.select_card = 0
                                 self.count.append(self.plant_setting)
                                 self.plant_setting = 0
+                                self.Planting_plant.set_volume(64)
                                 print(i)
                                 count = True
                                 break
